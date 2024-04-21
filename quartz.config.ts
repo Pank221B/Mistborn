e47262716f200d704c841b518e14631f7d0c92ee
Pank221B <trashcan55554444@gmail.com>
2024-04-21 02:27:44 -0500

    Quartz sync: Apr 21, 2024, 2:27 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -29,10 +29,10 @@ const config: QuartzConfig = {
       colors: {
         lightMode: {
           light: "#faf8f8",
-          lightgray: "#6b6b6b",
+          lightgray: "#30CDC6",
           gray: "#b8b8b8",
-          darkgray: "#4e4e4e",
-          dark: "#d1d1d1",
+          darkgray: "#ffffff",
+          dark: "#8EC6C3",
           secondary: "#284b63",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
